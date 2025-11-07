2ed41e31270175690f004045962d6e286d68a2c5
Abdulaziz <159067959+iazizlo@users.noreply.github.com>
2025-11-07 21:08:39 +0300

    Setting up the project

---
+++ officehour-backend/src/test/java/com/cpit252/officehour_backend/OfficehourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpit252.officehour_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficehourBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ officehourbooking-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <h1>Office hour Booking </h1> 
+    
+   
+    </div>
+  );
+}
+
+export default App;
